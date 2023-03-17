5f1df04afa11869d634a4add3f5787aa41b37722
GabiSilvaBelo <gabybelosp@gmail.com>
2023-03-17 09:26:10 -0300

    Inicializando Projeto Integrador 17/03/2023

---
+++ src/main/java/com/generation/donare/DonareApplication.java
@@ -0,0 +1,13 @@
+package com.generation.donare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/donare/DonareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.donare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
