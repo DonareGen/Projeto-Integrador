02e346cadf794e61d7b77be74bf23278027b19be
GabiSilvaBelo <gabybelosp@gmail.com>
2023-03-17 11:50:47 -0300

    Implementação das models

---
+++ src/main/java/com/generation/donare/model/Postagem.java
@@ -0,0 +1,106 @@
+package com.generation.donare.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Size(min = 5 , max = 100, message = "O título deve conter de 5 a 100 caracteres.")
+	private String titulo;
+	
+	@NotBlank(message = "O campo texto é obrigatório.")
+	@Size(min = 10, max = 1000, message = "O texto deve conter de 10 a 1000 caracteres.")
+	private String texto;
+	
+	private int curtidas;
+	
+	private String imagem;
+	
+	private String marcacao;
+	
+	private String doacao;
+	
+	/*@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema_id;*/
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public int getCurtidas() {
+		return curtidas;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public String getMarcacao() {
+		return marcacao;
+	}
+
+	public String getDoacao() {
+		return doacao;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public void setCurtidas(int curtidas) {
+		this.curtidas = curtidas;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public void setMarcacao(String marcacao) {
+		this.marcacao = marcacao;
+	}
+
+	public void setDoacao(String doacao) {
+		this.doacao = doacao;
+	}
+
+	/*public Tema getTema_id() {
+		return tema_id;
+	}
+
+	public void setTema_id(Tema tema_id) {
+		this.tema_id = tema_id;
+	}*/
+	
+	
+}
---
+++ src/main/java/com/generation/donare/model/Tema.java
@@ -0,0 +1,70 @@
+package com.generation.donare.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O campo nome é obrigatório.")
+	@Size(min = 5, max = 255, message = "O nome deve conter de 5 a 255 caracteres.")
+	private String nome;
+
+	@NotBlank(message = "O campo descrição é obrigatório")
+	@Size(min = 10, max = 255, message = "A descrição deve conter de 10 a 255 caracteres.")
+	private String descricao;
+	
+	/*@OneToMany(mappedBy ="tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagens;*/
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	/*public List<Postagem> getPostagens() {
+		return postagens;
+	}
+
+	public void setPostagens(List<Postagem> postagens) {
+		this.postagens = postagens;
+	}*/
+	
+	
+}
