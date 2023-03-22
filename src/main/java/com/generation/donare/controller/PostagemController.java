8208ccefdc64a6cafecfad372429744871388b09
joosilva <joaovictormendessilva1@gmail.com>
2023-03-22 14:24:16 -0300

    Implementação de PUT e DELETE para Http Request na Controller.

--- src/main/java/com/generation/donare/controller/PostagemController.java
+++ src/main/java/com/generation/donare/controller/PostagemController.java
@@ -1,17 +1,22 @@
 package com.generation.donare.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.donare.model.Postagem;
 import com.generation.donare.repository.PostagemRepository;
@@ -25,30 +30,44 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemRepository postagemRepository;
-	
+
 	@GetMapping
-	public ResponseEntity <List<Postagem>> getAll(){
-	 return ResponseEntity.ok(postagemRepository.findAll());
+	public ResponseEntity<List<Postagem>> getAll() {
+		return ResponseEntity.ok(postagemRepository.findAll());
 	}
 
-	
 	@GetMapping("/{id}")
-    public ResponseEntity <Postagem> getById(@PathVariable Long id){
-		return postagemRepository.findById(id).map(resposta-> ResponseEntity.ok(resposta))
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 
-   @GetMapping("buscaTexto/{texto}")
-   public ResponseEntity <List<Postagem>> getByTexto(@PathVariable String texto){
-	   return ResponseEntity.ok(postagemRepository.findAllByTextoContainingIgnoreCase(texto));
-   }
+	@GetMapping("buscaTexto/{texto}")
+	public ResponseEntity<List<Postagem>> getByTexto(@PathVariable String texto) {
+		return ResponseEntity.ok(postagemRepository.findAllByTextoContainingIgnoreCase(texto));
+	}
 
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
 
-  @PostMapping
-  public ResponseEntity <Postagem> post(@Valid @RequestBody Postagem postagem){
-	  return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
-  }
-       
-  
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+
+		if (postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+		postagemRepository.deleteById(id);
+	}
 
 }
